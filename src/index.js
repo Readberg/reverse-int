6bf29e34ff40c6d71421b56c3aa97be875dceef1
readberg <wayreadberg@gmail.com>
2022-11-25 21:13:13 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    n = n.toString().split('');
+    for (let i = 0; i < n.length; i++) {
+        if (n[i] === '-' || n[n.length] === '0'){
+            n[i] = '';
+        }
+    }
+    n = n.reverse().join('');
+    return +n;
 }
